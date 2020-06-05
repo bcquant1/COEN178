e0d163b43c4a65025c339ded89c42710e5d68fda
Brandon Quant <bquant@scu.edu>
2020-06-05 02:05:30 -0700

    Update tables_part1.sql

--- Lab2/tables_part1.sql
+++ Lab2/tables_part1.sql
@@ -1,11 +1,21 @@
-CREATE TABLE L_EMP(
-  empNo INTEGER PRIMARY KEY,
-  empname VARCHAR(10),
-  deptId VARCHAR(5)
+CREATE TABLE Customer(
+  custid VARCHAR(5) PRIMARY KEY,
+  firstname VARCHAR(10),
+  lastname VARCHAR(15),
+  city VARCHAR(10)
 );
 
-CREATE TABLE L_DEPT(
-  deptId VARCHAR(5) PRIMARY KEY,
-  deptname VARCHAR(10)
-);
+CREATE TABLE DeliveryService(
+  serviceid VARCHAR(10) PRIMARY KEY,
+  item VARCHAR(15),
+  location VARCHAR(15),
+  servicefee NUMBER(5,2)
+ );
 
+CREATE TABLE Schedule(
+  serviceid VARCHAR(10),
+  custid VARCHAR(5),
+  day VARCHAR(2) NOT NULL CHECK (day IN ('m','t','w','r','f')),
+  FOREIGN KEY (serviceid) REFERENCES DeliveryService(serviceid),
+  FOREIGN KEY (custid) REFERENCES Customer(custid)
+);
