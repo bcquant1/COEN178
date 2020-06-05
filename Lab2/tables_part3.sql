5189f9a29c4d42fc45b36efa8fbb1bd0d7c1537f
Brandon Quant <bquant@scu.edu>
2020-06-05 02:08:33 -0700

    Create tables_part3.sql

---
+++ Lab2/tables_part3.sql
@@ -0,0 +1,10 @@
+CREATE TABLE L_EMP(
+  empNo INTEGER PRIMARY KEY,
+  empname VARCHAR(10),
+  deptId VARCHAR(5)
+);
+
+CREATE TABLE L_DEPT(
+  deptId VARCHAR(5) PRIMARY KEY,
+  deptname VARCHAR(10)
+ );
